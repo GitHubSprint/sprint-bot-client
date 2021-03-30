9ce0f5095e2d3d154762518fda051fbd5012b6fc
Slawomir Kostrzewa <36227120+skostrze@users.noreply.github.com>
2021-03-30 22:12:39 +0200

    Update Main.java

--- src/main/java/pl/sprint/chatbot/client/Main.java
+++ src/main/java/pl/sprint/chatbot/client/Main.java
@@ -27,7 +27,7 @@ import pl.sprint.chatbot.client.test.TestThreadBotWithData;
  */
 public class Main
 {    
-    private final static String ENDPOINT =  "https://loclahost:8443/api";
+    private final static String ENDPOINT =  "https://localhost:8443/api";
     private final static String API_KEY = "Sprint";
     
     public static void main(String[] args) throws InterruptedException, IOException
