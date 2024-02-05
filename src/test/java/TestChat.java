8d25e4ad59c51b786d48d58690cfad14ff3ea5e4
Slawomir Kostrzewa <36227120+skostrze@users.noreply.github.com>
2024-02-05 12:22:43 +0100

    Update TestChat.java

--- src/test/java/TestChat.java
+++ src/test/java/TestChat.java
@@ -23,7 +23,9 @@ import java.util.concurrent.Executors;
 public class TestChat {
     private final String api_key;
     private final String endpoint;
-
+    private int cnt=0;
+    private int cntMore = 0;
+    private long avgTime = 0;
 
     public TestChat(String endpoint, String api_key) {
         this.endpoint = endpoint;
@@ -34,16 +36,20 @@ public class TestChat {
     public void start() throws Exception {
         SprintBotClient.disableSslVerification();
 
-        int numberOfTests = 500;
+        int numberOfTests = 10000;
         List<Callable<Void>> tasks = new ArrayList<>();
 
         for (int i = 0; i < numberOfTests; i++) {
             tasks.add(this::testCEZBot);
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(50);
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
         executorService.invokeAll(tasks);
         executorService.shutdown();
+
+        System.out.println("Counter: " + cnt);
+        System.out.println("Counter more than 1 sec: " + cntMore);
+        System.out.println("Average time: " + avgTime/cnt);
     }
 
 
@@ -53,16 +59,16 @@ public class TestChat {
 
         Session session = sprintBotClient.openSession(api_key, "testowa","CHAT","TEST", map,null);
 
-        System.out.println("Open session " + session.toString());
+//        System.out.println("Open session " + session.toString());
         map.put("session", session.getSessionId());
 
         sprintBotClient.updateData(session.getSessionId(), map);
 
         ChatBot cb = sprintBotClient.chat(session.getSessionId(), "ML", api_key, true);
-        System.out.println(cb);
-        Thread.sleep(1000);
+//        System.out.println(cb);
+        Thread.sleep(2000);
         cb = sprintBotClient.chat(session.getSessionId(), "TAK", api_key, true);
-        System.out.println("TAK" + " - " + cb.getText());
+//        System.out.println("TAK" + " - " + cb.getText());
         map = new HashMap<>();
         map.put("a", "Łódź");
         map.put("b", "Warszawa");
@@ -75,15 +81,18 @@ public class TestChat {
 
         sprintBotClient.updateData(session.getSessionId(), map);
         Map<String, String> sessionData = sprintBotClient.getSessionData(session.getSessionId());
-        System.out.println("Session Data: \n" + sessionData);
+        if(sessionData.isEmpty()) System.err.println("Empty MAP!!!");
+//        System.out.println("Session Data: \n" + sessionData);
         cb = sprintBotClient.chat(session.getSessionId(), "NIE", api_key, true);
-        System.out.println("NIE" + " - " + cb.getText());
+//        System.out.println("NIE" + " - " + cb.getText());
 
         cb = sprintBotClient.chat(session.getSessionId(), "TAK", api_key, true);
-        System.out.println("TAK" + " - " + cb.getText());
+//        System.out.println("TAK" + " - " + cb.getText());
 
         cb = sprintBotClient.chat(session.getSessionId(), "NIE", api_key, true);
-        System.out.println("NIE" + " - " + cb.getText());
+//        System.out.println("NIE" + " - " + cb.getText());
+
+        long startTime = System.currentTimeMillis();
         map = new HashMap<>();
         map.put("h", "Łódź");
         map.put("g", "Warszawa");
@@ -95,7 +104,7 @@ public class TestChat {
         map.put("a", "Żelechów");
         sprintBotClient.updateData(session.getSessionId(), map);
         sessionData = sprintBotClient.getSessionData(session.getSessionId());
-        System.out.println("Session Data: \n" + sessionData);
+        if(sessionData.isEmpty()) System.err.println("Empty MAP!!!");
 
         map = new HashMap<>();
         map.put("l", "Łódź");
@@ -108,11 +117,27 @@ public class TestChat {
         map.put("z", "Żelechów");
         sprintBotClient.updateData(session.getSessionId(), map);
         sessionData = sprintBotClient.getSessionData(session.getSessionId());
-        System.out.println("Session Data: \n" + sessionData);
+        if(sessionData.isEmpty()) System.err.println("Empty MAP!!!");
 
         sprintBotClient.updateData(session.getSessionId(), map);
+        sessionData = sprintBotClient.getSessionData(session.getSessionId());
+        if(sessionData.isEmpty()) System.err.println("Empty MAP!!!");
+        long endTime = System.currentTimeMillis();
+        long timeInMs = endTime - startTime;
+
+        avgTime = avgTime + timeInMs;
+
+        System.err.println(cnt + ": Time " + timeInMs + " ms");
+        if(timeInMs > 1000) {
+            System.out.println(cnt + ": More than 1 sec! time: " + timeInMs + " cntMore: " + cntMore);
+            cntMore++;
+        }
+        cnt++;
+        cb = sprintBotClient.chat(session.getSessionId(), "NIE", api_key, true);
+//        System.out.println("NIE" + " - " + cb.getText());
+
         session =  sprintBotClient.closeSession(session.getSessionId(),api_key, "testowa");
-        System.out.println("Close session " + session.toString());
+//        System.out.println("Close session " + session.toString());
         return null;
     }
     
